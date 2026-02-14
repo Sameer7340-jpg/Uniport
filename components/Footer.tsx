c83a7488efb7dcb8dc3a32120441caf426db548f
Sameer7340-jpg <aveskha11@gmail.com>
2026-02-14 23:45:39 +0530

    Add Footer component with copyright notice

---
+++ components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="text-center py-8 border-t mt-16">
+      <p>© {new Date().getFullYear()} UniPort. All rights reserved.</p>
+    </footer>
+  );
+}
